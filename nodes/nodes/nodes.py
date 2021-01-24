70d1d941578e2e4875628dd2d89100cbe93b044c
David Ó Laigheanáin <davidlynam071@gmail.com>
2021-01-24 22:56:30 +0000

    Fix log message formatting.

--- nodes/nodes/nodes.py
+++ nodes/nodes/nodes.py
@@ -18,7 +18,7 @@ def find_internal_nodes_num(tree: list) -> int:
         tree_set.remove(root_node)  # Remove root node entry as it points to outside the tree.
     except KeyError:
         log.debug(
-            f"Couldn't remove root node from tree as the node does not exist."
+            f"Couldn't remove root node from tree as the node does not exist. "
             f"root_node={root_node} tree={tree}"
         )
     return len(tree_set)
